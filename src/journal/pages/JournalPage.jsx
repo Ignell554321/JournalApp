a3c937f79b754121118e156d75029e14393f3ec7
miguel <miguelguevaraalejandro@gmail.com>
2022-07-29 14:04:18 -0500

    rutas - componentes - maquetacion de  registro - login

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material"
+
+import { JournalLayout } from "../layout/JournalLayout"
+
+export const JournalPage=()=>{
+
+    return(
+        <>
+        <JournalLayout>
+
+            <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam labore ab, eos porro sit nulla voluptas numquam a corrupti possimus esse animi quaerat soluta optio quam, saepe magni. Reprehenderit, facere.</Typography>
+
+        </JournalLayout>
+
+        
+        </>
+
+    )
+
+}
